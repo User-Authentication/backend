b71340f9f409dc7039612e197496d6b5abd2c273
ShashiBondi <bondishashank.nitt@gmail.com>
2024-11-12 01:14:51 +0530

    Enable CORS

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
