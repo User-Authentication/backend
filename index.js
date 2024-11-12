7f9c282eb921059d4220665fe96bd79ce0f9a3c2
BONDI SHASHANK <83707127+ShashiBondi@users.noreply.github.com>
2024-11-12 18:09:03 +0530

    Update cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://str8bat-assignment.netlify.app/signup",
   })
 );
 const PORT = process.env.PORT;
