b093cb4a53a9f61aa9f2d74586f6acdeb7826001
BONDI SHASHANK <83707127+ShashiBondi@users.noreply.github.com>
2024-11-12 19:27:23 +0530

    remove unwanted comments in auth file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-// middlewares/auth.js
+
 import jwt from "jsonwebtoken";
 
 function auth(req, res, next) {
@@ -9,7 +9,7 @@ function auth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach user information to the request
+    req.user = decoded; 
     next();
   } catch (error) {
     res.status(400).json({ error: "Invalid token." });
